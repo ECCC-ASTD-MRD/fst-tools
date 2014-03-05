dac7af06c4014d1389d96af47ca0f10d23d15859
Michel <armnmfv@nemesis.(none)>
2014-03-05 14:04:41 -0500

    no longer using .cdk files, documenting, removing dependencies for def1/def2

--- editfst/configuration.f90
+++ editfst/configuration.f90
@@ -1,13 +1,15 @@
 module configuration
+!     internal variables used all over editfst
+!
       implicit none
       integer, private :: i
-      integer, parameter :: NCCARDKEYS=146
       integer, PARAMETER :: NMR=12, NMS=25, NME=20, NMN=40, NMM=10, NMD=20
       character(len=*), parameter :: LIN128 = '(32A4)'
-
-!      COMMON /CHAR/  NS, ND, SNOM, DNOM, ZE, ETI, ETIS(10,NMD),       &
-!                     ZT, TYP, TYPS(10,NMD), GTY, GTYS(NMD), GTYPS,    &
-!                     ZN, NOM, NOMS(10,NMD), ETAT
+!
+! NOTE:
+!     le nombre maximum de valeurs dans une liste de desiderata
+!     est limite (hardcoded) a 10 ( a corriger eventuellement)
+!
       character *1   , save :: GTY, GTYPS=' ', GTYS(NMD)
       character *2   , save :: ZT, TYP, TYPS(10,NMD)
       character *4   , save :: ZN, NOM, NOMS(10,NMD)
@@ -16,9 +18,15 @@ module configuration
       character *128 , save :: NS=' ', ND
       character *15  , save :: SNOM, DNOM
 
-      character(len=4096) ,dimension(:), pointer, save:: DEF1,     DEF2
+      character(len=4096) ,dimension(:), pointer, save:: def1,     def2
       character(len=4096), save :: PRINTR
+      integer, parameter :: NCCARDKEYS=146   ! dimension for program options (processed by ccard)
       character(len=8), dimension(NCCARDKEYS), save ::    &
+      kle = (/ 'NNN     ', 'D:      ', 'EOF     ', 'SSEQ    ', 'DSEQ    ', 'VD      ', &
+               'NOBOX   ', 'DIAG    ', 'ECR     ', 'I.      ', 'L.      ', 'K       ', &
+               'M       ', 'T       ', 'C       ', 'SS      ', 'DS      ', 'V       ', &
+               'N       ', 'VS      ', 'E       ', 'F       ', 'SF      ', 'DF      ', &
+               'NRECMIN ',('S:      ',i = 1,120),  'DRYRUN  ' /),                      &
       def1b=(/ 'OUI     ', '        ', '0       ', 'NON     ', 'NON     ', 'NON     ', &
                'NON     ', 'NON     ', 'NON     ', '$IN     ', '$OUT    ', 'FATALE  ', &
                'ERRORS  ', 'FATALE  ', '-1      ', 'NON     ', 'NON     ', 'NON     ', &
@@ -28,20 +36,15 @@ module configuration
                'OUI     ', 'OUI     ', 'OUI     ', '$IN     ', '$OUT    ', 'ERRORS  ', &
                'INFORM  ', 'ERRORS  ', '-1      ', 'OUI     ', 'OUI     ', 'OUI     ', &
                'OUI     ', 'OUI     ', 'OUI     ', 'OUI     ', 'OUI     ', 'OUI     ', &
-               '-1      ',('        ',i = 1,120),  'DRYRUN  '/),                       &
-      kle = (/ 'NNN     ', 'D:      ', 'EOF     ', 'SSEQ    ', 'DSEQ    ', 'VD      ', &
-               'NOBOX   ', 'DIAG    ', 'ECR     ', 'I.      ', 'L.      ', 'K       ', &
-               'M       ', 'T       ', 'C       ', 'SS      ', 'DS      ', 'V       ', &
-               'N       ', 'VS      ', 'E       ', 'F       ', 'SF      ', 'DF      ', &
-               'NRECMIN ',('S:      ',i = 1,120),  'DRYRUN  ' /)
-
+               '-1      ',('        ',i = 1,120),  'DRYRUN  '/)
+! table utilisee pour stocker les requetes
       integer*8, save :: JOURS(4),REQ(11,4,NMD)
 ! si REQ(11,...) = -1
 !     REQ(1,...)  valeur de depart
 !     REQ(2,...)  valeur de fin
 !     REQ(3,...)  delta
 ! sinon  REQ(11,...) = nombre de valeurs a utiliser
-!     REQ(1:REQ(11,...),....) liste des valeurs recherchees
+!     REQ(1:REQ(11,...),....) liste des valeurs recherchees (maximum de 10 valeurs)
 !     req(,1,) IP1
 !     req(,2,) IP2
 !     req(,3,) IP3
@@ -59,26 +62,6 @@ module configuration
                        INTERAC=.false., ZA=.false., DRYRUN=.false.,     &
                        FIXD=.false., ECR, SSEQ=.false., VS=.false.,             &
                        OUVS=.false., DSEQ, VD, OUVD=.false.
-!      DATA KLE /'NNN', 'D:', 'EOF', 'SSEQ', 'DSEQ', 'VD', 'NOBOX',
-!     X          'DIAG', 'ECR', 'I.', 'L.', 'K', 'M', 'T', 'C', 'SS',
-!     X          'DS', 'V', 'N', 'VS', 'E', 'F', 'SF', 'DF',
-!     X          'NRECMIN', 120*'S:','DRYRUN'/
-!      DATA DEF1b/'OUI', ' ', '0', 'NON', 'NON', 'NON', 'NON', 'NON', 
-!     X          'NON', '$IN', '$OUT', 'FATALE', 'ERRORS', 'FATALE',
-!     X          '-1',  'NON', 'NON', 'NON', 'NON', 'NON', 'NON', 'OUI',
-!     X          'NON', 'NON', '-1', 120*' ',' '/
-!      DATA DEF2b/'OUI', ' ', '0', 'OUI', 'OUI', 'OUI', 'OUI', 'OUI', 
-!     X          'OUI', '$IN', '$OUT', 'ERRORS', 'INFORM', 'ERRORS',
-!     X          '-1',  'OUI', 'OUI', 'OUI', 'OUI', 'OUI', 'OUI', 'OUI',
-!     X          'OUI', 'OUI', '-1', 120*' ','DRYRUN'/
-!      DATA VS, OUVS, OUVD, FIXD, DEBUG, XPRES, SCRI, ESAIS, INTERAC, ZA, DRYRUN
-!     X    /11 * .FALSE./
-!      DATA JOURS, DESEXC, SATISF,  NS,    ETAT
-!     X    /  4*0,  NMD*0,  NMD*0, ' ', 'NORMAL'/
-!      DATA NREQ, NEXC, CEOF, SAUV, MEOF, LEOF, NFSO
-!     X    /   0,    0,    0,    0,    1,    0,    0/
-!      DATA NFS, NIS, NJS, NKS, GTYPS, IG1S, IG2S, IG3S, IG4S
-!     X    /  0,  -1,  -1,  -1,   ' ',   -1,   -1,   -1,   -1/
 contains
 subroutine config_init
   req     = 0
