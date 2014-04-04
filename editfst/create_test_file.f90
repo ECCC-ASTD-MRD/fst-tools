7a3128af602dda8979532fed1d51b5083f9abda1
Michel <armnmfv@nemesis.(none)>
2014-04-04 12:26:48 -0400

    test problems

--- editfst/create_test_file.f90
+++ editfst/create_test_file.f90
@@ -42,7 +42,7 @@ integer, external :: fstlir
 print *,'creating standard file for editfst test'
 call fnom(10,'test.fst','STD+RND',0)
 call fstouv(10,'RND')
-call fstopc("PRINTOPT","NINJNK+DATESTAMPO+IPALL+IG1234",0)
+call fstopc("PRINTOPT","NINJNK+DATESTAMPO+IPALL+NOIP23+IG1234",0)
 kind1a = 2  ! millibars
 kind1b = 4  ! meters above ground
 kind1c = 6  ! theta
@@ -55,9 +55,10 @@ do i=1,NJD
   array(i,j) = sqrt( (i-5.5)**2 + (j-5.5)**2 )/6.364
 enddo
 enddo
-do j=NJD,1,-1
-  print 111,array(:,j)
+do ii=NJD,1,-1
+  print 111,array(:,ii)
 enddo
+print *,'============================================'
 do j=1,4
   do i=0,24,6
     do k=200,1000,400
@@ -65,32 +66,28 @@ do j=1,4
       p2 = i
       p3 = j+i
       status =  encode_ip(ip1,ip2,ip3,p1,kind1a,p2,kind2,p3,kind3)
-      array=p3
+!      array=p3
     !  print *,status,ip1,kind_to_string(kind1),ip2,kind_to_string(kind2),ip3,kind_to_string(kind3)
       write(etiket,1)'ETIKET',mod(j+i,7)
     1 format(A,I6.6)
-      call fstecr(array,work,NBITS,10,0,0,0,10,10,1,ip1,ip2,ip3,'XX',name1(j),etiket,'X',0,0,0,0,TYP134,.false.)
+      call fstecr(array,work,NBITS,10,0,0,0,NID,NJD,1,ip1,ip2,ip3,'XX',name1(j),etiket,'X',0,0,0,0,TYP134,.false.)
       status=fstlir(work,10,ni,nj,nk,-1,etiket,ip1,ip2,ip3,'XX',name1(j))
-do ii=NJD,1,-1
-  print 111,array(:,ii)
-enddo
-      if(array(1,1)>1.1 .or. work(1,1)>1.1) goto 20
       status =  encode_ip(ip1,ip2,ip3,p1,kind1b,p2,kind2,p3,kind3)
-      call fstecr(array,work,NBITS,10,0,0,0,10,10,1,ip1,ip2,ip3,'XX',name1(j),etiket,'X',0,0,0,0,TYP134,.false.)
+      call fstecr(array,work,NBITS,10,0,0,0,NID,NJD,1,ip1,ip2,ip3,'XX',name1(j),etiket,'X',0,0,0,0,TYP134,.false.)
       status =  encode_ip(ip1,ip2,ip3,p1,kind1c,p2,kind2,p3,kind3)
-      call fstecr(array,work,NBITS,10,0,0,0,10,10,1,ip1,ip2,ip3,'XX',name1(j),etiket,'X',0,0,0,0,TYP134,.false.)
+      call fstecr(array,work,NBITS,10,0,0,0,NID,NJD,1,ip1,ip2,ip3,'XX',name1(j),etiket,'X',0,0,0,0,TYP134,.false.)
       status =  encode_ip(ip1,ip2,ip3,p1*.001,kind1d,p2,kind2,p3,kind3)
-      call fstecr(array,work,NBITS,10,0,0,0,10,10,1,ip1,ip2,ip3,'XX',name1(j),etiket,'X',0,0,0,0,TYP134,.false.)
+      call fstecr(array,work,NBITS,10,0,0,0,NID,NJD,1,ip1,ip2,ip3,'XX',name1(j),etiket,'X',0,0,0,0,TYP134,.false.)
     enddo
   enddo
   ip1=63240+(j-1)*100 ; ip2=0 ; ip3 = 0
-  call fstecr(array,work,NBITS,10,0,0,0,10,10,1,ip1,ip2,ip3,'YY',name2(j),etiket,'X',0,0,0,0,TYP134,.false.)
+  call fstecr(array,work,NBITS,10,0,0,0,NID,NJD,1,ip1,ip2,ip3,'YY',name2(j),etiket,'X',0,0,0,0,TYP134,.false.)
 enddo
-call fstecr(array,work,NBITS,10,0,0,0,10,10,1,63540,0,0,'XX','HHHH',etiket,'X',0,0,0,0,TYP6,.false.)
+call fstecr(array,work,NBITS,10,0,0,0,NID,NJD,1,63540,0,0,'XX','HHHH',etiket,'X',0,0,0,0,TYP6,.false.)
 do j=NJD,1,-1
   print 111,array(:,j)
 enddo
-111 format(10F8.3)
+111 format(10F6.2,5x,10F6.2)
 goto 20
 
 10 continue   ! large record test
