6f52cb145f35e5c5924b478ff973bccae02b941c
Michel Valin <mfvalin@gmail.com>
2015-04-02 07:54:57 -0400

    acquired missing rmnlib beta code

---
+++ editfst/qstdir.h
@@ -0,0 +1,1 @@
+EXTRAS/qstdir.h
--- editfst/run_tests.sh
+++ editfst/run_tests.sh
@@ -3,12 +3,12 @@
 [[ ! -r $2 ]] && echo "test file '$2' not found or no readable"
 echo "running tests for '$1' using input file '$2'"
 ulimit -s 16384000
-for dirfile in *.dir
+for dirfile in test*.dir
 do
   [[ -r $dirfile ]] || break
   echo "================== $dirfile =================="
   cat $dirfile
-  echo "executing: ./$1 -s $2 -i $dirfile -dryrun"
+  echo "executing: ./$1 -s $2 -i $dirfile -dryrun -nobox -d rien.fst"
   ./$1 -s $2 -i $dirfile -dryrun -nobox -d rien.fst
 done
 echo "================== TESTS END =================="
