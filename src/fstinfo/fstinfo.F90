db6edc69295accf627113d669e12454becae3a24
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@ec.gc.ca>
2024-06-05 16:51:55 +0000

    Added back extra legacy parameters, mostrly for positioning of extra1 which is datev datestamp Added -meta flag to output new json metadata

--- src/fstinfo/fstinfo.F90
+++ src/fstinfo/fstinfo.F90
@@ -28,14 +28,14 @@
       character(len=12) :: ptetiket
 
       character(len=8) :: cltimev
-      character(len=8) :: ptcle(12)
-      character(len=4096) :: ptvar(12), ptdefvar(12)
+      character(len=8) :: ptcle(13)
+      character(len=4096) :: ptvar(13), ptdefvar(13)
 
-      data ptcle /'izfst.','datev.','vdatev.','etiket.','ip1.','ip2.','ip3.','typvar.','nomvar.','otxt.','del.','champs.'/
-      data ptvar /'bidon','-1','-1',' ','-1','-1','-1',' ',' ','fstlist',':','0'/
-      data ptdefvar /'bidon','-1','-1',' ','-1','-1','-1',' ',' ','fstlist',':','1'/
+      data ptcle /'izfst.','datev.','vdatev.','etiket.','ip1.','ip2.','ip3.','typvar.','nomvar.','otxt.','del.','champs.','meta.'/
+      data ptvar /'bidon','-1','-1',' ','-1','-1','-1',' ',' ','fstlist',':','0','0'/
+      data ptdefvar /'bidon','-1','-1',' ','-1','-1','-1',' ',' ','fstlist',':','1','1'/
 
-      call ccard(ptcle,ptdefvar,ptvar,12,-1)
+      call ccard(ptcle,ptdefvar,ptvar,13,-1)
 
 !     ------ Liste de champs -----
       if (ptvar(12).eq.'1') then
@@ -46,21 +46,21 @@
               '3 ip1',               &
               '4 ip2',               &
               '5 ip3',               &
-              '6  ni',               &
-              '7  nj',               &
-              '8  nk',               &
+              '6 ni',                &
+              '7 nj',                &
+              '8 nk',                &
               '9 etiket',            &
-              '10  date d origine',  & 
+              '10 date d origine',   & 
               '11 date de validite', &
               '12 deet',             &
-              '13  npas',            &
+              '13 npas',             &
               '14 grtyp',            &
               '15 ig1',              &
               '16 ig2',              &
               '17 ig3',              &
               '18 ig4',              &
-              '19  datyp',           &
-              '20  nbits',           &
+              '19 datyp',            &
+              '20 nbits',            &
               '21 swa',              &
               '22 lng',              &
               '23 dltf',             &
@@ -68,7 +68,7 @@
               '25 extra1',           &
               '26 extra2',           &
               '27 extra3',           &
-              '28 json' 
+              '28 metadata' 
          stop
       endif
 
@@ -82,6 +82,7 @@
          app_status=app_end(-1)
          call qqexit(app_status)
       endif
+
 !     ------ Initialisation des clefs de recherche -----
 
       if (ptvar(2) .ne. '-1') then
@@ -105,32 +106,41 @@
       pttypvar=ptvar(8)
       ptnomvar=ptvar(9)
       ptdel=ptvar(11)
-
+ 
 !     ------ Ouverture des fichiers ------
 
       pnseqout = 21
       open(pnseqout,FILE=ptvar(10),ACCESS='SEQUENTIAL')
 
       success = source%open(trim(ptvar(1)),'STD+R/O+REMOTE')
-      query = source%new_query(datev=int(pndatev, int64), etiket=ptetiket, ip1=pnip1, ip2=pnip2, ip3=pnip3, typvar=pttypvar, nomvar=ptnomvar)
+      query = source%new_query(datev=pndatev, etiket=ptetiket, ip1=pnip1, ip2=pnip2, ip3=pnip3, typvar=pttypvar, nomvar=ptnomvar)
 
       do while(query%find_next(record))
  
          call newdate(record%dateo,pndatprinto,pntimeo,-3)
          call newdate(record%datev,pndatprintv,pntimev,-3)
 
-         success = record%read_metadata()  
-         write (pnseqout,1000) record%nomvar,ptdel,record%typvar,ptdel,record%ip1,ptdel  &
-              ,record%ip2,ptdel,record%ip3,ptdel,record%ni,ptdel,record%nj,ptdel,record%nk      &
-              ,ptdel,record%etiket,ptdel,pndatprinto,pntimeo,ptdel             &
-              ,pndatprintv,pntimev,ptdel,record%deet,ptdel,record%npas,ptdel        &
-              ,record%grtyp,ptdel,record%ig1,ptdel,record%ig2,ptdel,record%ig3,ptdel,record%ig4    &
-              ,ptdel,record%data_type,ptdel,record%data_bits,ptdel,record%metadata%stringify(JSON_C_TO_STRING_SPACED)
-     end do
-
+         if (ptvar(13).eq.'1') then
+            success = record%read_metadata()  
+            write (pnseqout,1001) record%nomvar,ptdel,record%typvar,ptdel,record%ip1,ptdel,record%ip2,ptdel,record%ip3 &
+                  ,ptdel,record%ni,ptdel,record%nj,ptdel,record%nk,ptdel,record%etiket,ptdel,pndatprinto,pntimeo        &
+                  ,ptdel,pndatprintv,pntimev,ptdel,record%deet,ptdel,record%npas                                        &
+                  ,ptdel,record%grtyp,ptdel,record%ig1,ptdel,record%ig2,ptdel,record%ig3,ptdel,record%ig4               &
+                  ,ptdel,record%data_type,ptdel,record%data_bits,ptdel,0,ptdel,0                                        &
+                  ,ptdel,0,ptdel,0,ptdel,record%datev,ptdel,0,ptdel,0,ptdel,record%metadata%stringify(JSON_C_TO_STRING_SPACED)
+         else
+            write (pnseqout,1000) record%nomvar,ptdel,record%typvar,ptdel,record%ip1,ptdel,record%ip2,ptdel,record%ip3 &
+                  ,ptdel,record%ni,ptdel,record%nj,ptdel,record%nk,ptdel,record%etiket,ptdel,pndatprinto,pntimeo        &
+                  ,ptdel,pndatprintv,pntimev,ptdel,record%deet,ptdel,record%npas                                        &
+                  ,ptdel,record%grtyp,ptdel,record%ig1,ptdel,record%ig2,ptdel,record%ig3,ptdel,record%ig4               &
+                  ,ptdel,record%data_type,ptdel,record%data_bits,ptdel,0,ptdel,0                                        &
+                  ,ptdel,0,ptdel,0,ptdel,record%datev,ptdel,0,ptdel,0
+         endif
+      end do
       success = source%close()
 
- 1000 format(2(a,a),6(i10,a),1(a,a),2(i8.8,i8.8,a),2(i10,a),1(a,a),6(i10,a),a)
+ 1000 format(2(a,a),6(i10,a),1(a,a),2(i8.8,i8.8,a),2(i10,a),1(a,a),13(i10,a))
+ 1001 format(2(a,a),6(i10,a),1(a,a),2(i8.8,i8.8,a),2(i10,a),1(a,a),13(i10,a),a)
 
       app_status=app_end(-1);
       call qqexit(app_status)
