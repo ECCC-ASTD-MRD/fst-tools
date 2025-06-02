1af905167151525e64d6d0baf11087186106af6f
Vincent Magnoux <vincent.magnoux@ec.gc.ca>
2025-06-02 19:57:16 +0000

    [tests] Specify editfst and fstcomp executables for editfst test

--- tests/editfst/01_editfst_basic.sh
+++ tests/editfst/01_editfst_basic.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+if [ $# -lt 2 ]; then
+    echo "Need arguments to specify editfst and fstcomp executables"
+    exit -1
+fi
+
+EDITFST="${1}"
+FSTCOMP="${2}"
+
+echo "EDITFST = ${EDITFST}"
+echo "FSTCOMP = ${FSTCOMP}"
+
 function run_test() {
     test_file=${1:-test.fst}
     pwd
@@ -14,22 +25,22 @@ function run_test() {
 
     output_file_1=${test_file}.01
     rm -f ${output_file_1}
-    editfst -s ${test_file} -d ${output_file_1} -i ${directives} || exit -1
+    ${EDITFST} -s ${test_file} -d ${output_file_1} -i ${directives} || exit -1
 
     output_file_2=${test_file}.02
     rm -f ${output_file_2}
-    cat ${directives} | editfst -s ${test_file} -d ${output_file_2} || exit -1
+    cat ${directives} | ${EDITFST} -s ${test_file} -d ${output_file_2} || exit -1
 
-    fstcomp -a ${output_file_1} -b ${output_file_2} -ecode || exit -1
-    fstcomp -a ${output_file_2} -b ${output_file_1} -ecode || exit -1
+    ${FSTCOMP} -a ${output_file_1} -b ${output_file_2} -ecode || exit -1
+    ${FSTCOMP} -a ${output_file_2} -b ${output_file_1} -ecode || exit -1
 
     output_file_3=${test_file}.03
     rm -f ${output_file_3}
-    editfst -s ${test_file} -d ${output_file_3} -i 0 || exit -1
-    fstcomp -a ${test_file} -b ${output_file_3} -ecode || exit -1
+    ${EDITFST} -s ${test_file} -d ${output_file_3} -i 0 || exit -1
+    ${FSTCOMP} -a ${test_file} -b ${output_file_3} -ecode || exit -1
 }
 
-fstcomp -a test.rsf -b test.xdf -ecode
+${FSTCOMP} -a test.rsf -b test.xdf -ecode
 
 run_test test.rsf
 run_test test.xdf
