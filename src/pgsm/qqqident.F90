c489098ac4e98d3f9e72bb71218b12b68c98b19d
Samuel Gilbert <samuel.gilbert@ec.gc.ca>
2026-01-21 22:08:25 +0000

    [pgsm] Fix logic bug in ident directive processing
    - Make local C functions static
    - Remove inclusion of unused cdk90 files

--- src/pgsm/qqqident.F90
+++ src/pgsm/qqqident.F90
@@ -1,47 +1,45 @@
-#include "defin.cdk90"
-      subroutine qqqident(position, separateur, items)
-   implicit none
+subroutine qqqident(position, separateur, items)
+    implicit none
 
-      integer position, separateur, items(16)
+    integer, intent(in) :: position
+    integer, intent(in) :: separateur
+    integer, intent(in) :: items(16)
 
-      integer argdims
-      external argdims
+    integer, external :: argdims
 
-#include "champs.cdk90"
+! qcsepar, qitems, qnitems, qposition
 #include "idents.cdk90"
 
-      integer i
+    integer i
 
-!      print *, 'qqqident', position, separateur
-!      do i=1,16
-!        print *, i, items(i)
-!      enddo
+    ! print *, 'qqqident', position, separateur
+    ! do i = 1, 16
+    !     print *, i, items(i)
+    ! enddo
 
-      qposition = position
-      if (qposition.eq.5) then
-         qnitems = 0
-      endif
+    qposition = position
+    if (qposition == 5) then
+        qnitems = 0
+    endif
 
-      if (separateur.eq.-1) then
-         qcsepar = 'T'
-      else
-         write(qcsepar,'(a1)') separateur
-      endif
-      
-      do i=1,16
-         qitems(i) = 0
-      enddo
+    if (separateur == -1) then
+        qcsepar = 'T'
+    else
+        write(qcsepar,'(a1)') separateur
+    endif
 
-      if (-1.eq.items(1)) then
-         qnitems = 11
-         do i=1,qnitems
+    do i = 1, 16
+        qitems(i) = 0
+    enddo
+
+    if (-1 == items(1)) then
+        qnitems = 11
+        do i = 1, qnitems
             qitems(i) = i
-         enddo
-      else
-         do i=1,argdims(3)
+        enddo
+    else
+        do i = 1, argdims(3)
             qitems(i) = items(i)
-         enddo
-      endif
-      
-      return
-      end
+        enddo
+    endif
+end
