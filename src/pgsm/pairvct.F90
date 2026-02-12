1d47a6499e97d5d95efdb0c0daa9a062ca5077fe
Samuel Gilbert <samuel.gilbert@ec.gc.ca>
2026-02-12 22:53:13 +0000

    Fix potential crash with '/' in format

--- src/pgsm/pairvct.F90
+++ src/pgsm/pairvct.F90
@@ -1,7 +1,5 @@
-!
 !**S/P PAIRVCT  REMPLACE OU AJOUTE NOM AU DICTIONNAIRE COMMON/PAIR/...
-!
-   subroutine pairvct(nomusag, varuu, varvv, varmodule, vardir) 
+subroutine pairvct(nomusag, varuu, varvv, varmodule, vardir)
       use app
       implicit none 
 !
@@ -46,57 +44,51 @@
 !           'PAIRES("UV","UU","VV","0") 
 !           'PAIRES("VENTUVS","US","VS","UV")
 !           'PAIRES("UVS","UU","VV","0")
-!
-!IMPLICITES
-!MODULES
-!
-!
 !- - -- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-!
+
 #include "defin.cdk90"
 #include "pairs.cdk90"
-!
-!
+
       integer nomusag(2),varuu,varvv,varmodule,vardir
       character*8 cnomusr
       character*4 cvaruu, cvarvv, ccontrl, cvarwd
       integer i, nw
       integer argdims
-      
+
       external fstcvt
       integer  fstcvt
-      
+
       if (npairuv.lt.3.or.npairuv.gt.5) then
          call app_log(APP_ERROR,'pairvct: Check PAIRES directives for 3 or 4 arguments')
          return
       endif
-!
+
 !   VERIFI SI NOM EXISTE DANS LA TABLE SI OUI ON REMPLACE
 !   SI NON ON AJOUTE SI LA TABLE N'EST PAS PLEINE 
 !
       nw = min(argdims(1), 2) 
       write (cnomusr, 100) (nomusag(i), i=1,nw)
  100  format(2a4)
-      
+
       write (cvaruu, 200) varuu
       write (cvarvv, 200) varvv
       write (ccontrl, 200) varmodule
       write (cvarwd, 200) vardir
  200  format(a4)
-      
+
       if (varmodule.eq.0) ccontrl = '??'
       if (vardir.eq.0) cvarwd = '??'
-      
+
       write (app_msg,*) 'pairvct: PAIRES: ',cnomusr, cvaruu, cvarvv, ccontrl, cvarwd
       call app_log(APP_INFO,app_msg)
-      
+
       call pairvc2(cnomusr, cvaruu, cvarvv, ccontrl, cvarwd)
       return
       end 
-  
-!**S/P PAIRVC2  REMPLACE OU AJOUTE NOM AU DICTIONNAIRE COMMON/PAIR/...
-      subroutine pairvc2(cnomusr,cvaruu,cvarvv,ccontrl,cvarwd)
-!
+
+! REMPLACE OU AJOUTE NOM AU DICTIONNAIRE COMMON/PAIR/...
+subroutine pairvc2(cnomusr,cvaruu,cvarvv,ccontrl,cvarwd)
+
 !AUTEUR P. SARRAZIN DORVAL QUE CANADA FEV 87
 !
 !REVISION 4.0.2
@@ -137,24 +129,20 @@
 !           'PAIRES("UV","UU","VV","0") 
 !           'PAIRES("VENTUVS","US","VS","UV")
 !           'PAIRES("UVS","UU","VV","0")
-!
-!MODULES
-!
-!
 !-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-!
+
       use app
       implicit none
 #include "defin.cdk90"
 #include "pairs.cdk90"
-!
+
       character cnomusr*8, cvaruu*4 , cvarvv*4 , ccontrl*4 , cvarwd*4 
       integer np
       logical remplac
-!     
+
 !   VERIFI SI NOM EXISTE DANS LA TABLE SI OUI ON REMPLACE
 !   SI NON ON AJOUTE SI LA TABLE N'EST PAS PLEINE 
-!
+
       remplac=.false.
       do np=1,npair
          if (cnomusr.eq.paire(np)(1:8)) then
@@ -168,19 +156,18 @@
             call app_log(APP_INFO,app_msg)
              endif
       enddo
-!     
+
       if (remplac)  go to 1000
-!
-!   SI ON N'A PAS REMPLACE DANS LA TABLE ON AJOUTE
-!
+
+      ! SI ON N'A PAS REMPLACE DANS LA TABLE ON AJOUTE
       npair = npair + 1
       if (npair.gt.NPAIRMX) then
          write(app_msg,666) npair,npairmx
          call app_log(APP_ERROR,app_msg)
- 666     format(1x,'pairvct: Too many pairs in table  NPAIR=',i5,         /'   NPAIRMX=',i5)
+ 666     format(1x, 'pairvct: Too many pairs in table NPAIR=', i5, ' NPAIRMX=', i5)
          return
       endif
-!
+
       paire(np)( 1: 8) = cnomusr 
       paire(np)( 9:12) = cvaruu 
       paire(np)(13:16) = cvarvv 
@@ -188,11 +175,7 @@
       paire(np)(21:24) = cvarwd
       write (app_msg,*) 'pairvct: PAIRE(NP): ', paire(np)
       call app_log(APP_INFO,app_msg)
-return 
-!
-!
+
  1000  call app_log(APP_INFO,'pairvct: 2 pair variables replaced')
-!     
-      return 
-      end
+end
 
